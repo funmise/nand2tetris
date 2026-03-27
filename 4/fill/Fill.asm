39500056eed73340598c1128bd12a245cfafb3d5
funmise <42499166+funmise@users.noreply.github.com>
2026-03-27 04:40:04 -0600

    Implement Screen Fill script

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -7,4 +7,63 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+    // col = 0
+    @col
+    M=0
+
+    // Check if key pressed
+(CHECK)
+    @n
+    M=0
+    @KBD
+    D=M
+    @SETBLACK
+    D;JGT
+    @SETCLEAR
+    D;JEQ
+
+    //if key presssed, col = -1
+(SETBLACK)
+    @col
+    M=-1
+    @COLOR
+    0;JMP
+
+    // else col = 0
+(SETCLEAR)
+    @col
+    M=0
+    
+    // set to black or clear every word(pixels) on screen
+(COLOR)
+     // if n == 8192 goto CHECK
+    @n
+    D=M
+    @8192
+    D=D-A
+    @CHECK
+    D;JEQ
+
+    // address = SCREEN + n
+    @SCREEN
+    D=A
+    @n
+    D=D+M
+    @addr
+    M=D
+
+    // D = col value
+    @col
+    D=M
+
+    // *addr = col
+    @addr
+    A=M
+    M=D
+
+    // n += 1
+    @n
+    M=M+1
+    @COLOR
+    0;JMP
+
